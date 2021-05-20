b9da04c6b8e80cee727a48e78368221cdfbc8cec
Bradley <bmcguigandev@outlook.com>
2021-05-20 12:19:06 +0100

    Allow resizing of sketchpad

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,43 @@
-const tilesContainer = document.querySelector('.tiles-container');
+const tileGrid = document.querySelector('.tile-grid');
+const gridSizeInput = document.querySelector('.grid-size-input');
+const gridSizeDisplay = document.querySelector('.grid-size-display');
+const gridSizeButton = document.querySelector('.grid-size-button');
 
-for (let i = 0; i < 256; i++) {
-    let tile = document.createElement('div');
-    tile.classList.add('tile');
-    tilesContainer.appendChild(tile);
+initializeGrid(tileGrid, 256);
+initializeControls();
+gridSizeButton.addEventListener('click', updateGrid);
+
+
+function updateGrid() {
+    initializeGrid(tileGrid, gridSizeInput.value ** 2);
+    tileGrid.style.gridTemplateColumns = `repeat(${gridSizeInput.value}, 1fr)`;
+    tileGrid.style.gridTemplateRows = `repeat(${gridSizeInput.value}, 1fr)`;
 }
 
-let tiles = document.querySelectorAll('.tile');
+function initializeGrid(grid, tileCount) {
+    grid.innerHTML = '';
+    fillContainerWithTiles(tileCount);
+    makeTilesFillable();
+}
 
-tiles.forEach(tile => tile.addEventListener('mouseover', () => {
-    tile.style.backgroundColor = '#b8b8b8';
-}));
+function fillContainerWithTiles(tileCount) {
+    for (let i = 0; i < tileCount; i++) {
+        let tile = document.createElement('div');
+        tile.classList.add('tile');
+        tileGrid.appendChild(tile);
+    }
+}
+
+function makeTilesFillable() {
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach(tile => tile.addEventListener('mouseover', () => {
+        tile.style.backgroundColor = '#b8b8b8';
+    }));
+}
+
+function initializeControls() {
+    gridSizeDisplay.textContent = `${gridSizeInput.value} x ${gridSizeInput.value}`;
+    gridSizeInput.addEventListener('input', () => {
+        gridSizeDisplay.textContent = `${gridSizeInput.value} x ${gridSizeInput.value}`;
+    });
+}
